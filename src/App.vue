8c8c7d056f19d87bc0213c23941fd867a34093da
aegorsuch <alex@weartak.com>
2026-02-21 20:33:36 -0600

    remove weapon name

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
-computed: {
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    range: 'melee',
+    adjustements: {
+      proficiency: 0,
+      die: 0,
+      hands: 0,
+      reload: 0,
+      volley: 0,
+      range: 0,
+    },
+    traits: {
+      onePoint: [],
+      twoPoint: [],
+      threePoint: [],
+    },
+    selectedAncestry: '',
+  }),
+  computed: {
     isMelee () {
       return this.range === 'melee';
     },
     total () {
-      // 1. Start with the "Bonus Point"
       let bonusPoint = 2;
-
-      // 2. Calculate the "Cost" of the base weapon
       let baseCost = this.adjustements.proficiency + this.adjustements.die + this.adjustements.hands;
       
-      // 3. Apply Ranged penalties if applicable
       if(!this.isMelee) {
         baseCost = baseCost - 3 + this.adjustements.reload + this.adjustements.volley + this.adjustements.range;
       }
 
-      // 4. Subtract the cost of selected traits
       let traitsCost = this.traits.onePoint.length + 
                        this.traits.twoPoint.length * 2 + 
                        this.traits.threePoint.length * 3;
 
-      // Final Math: Bonus + Base Stats - Traits
       return bonusPoint + baseCost - traitsCost;
     },
     allTraits() {
@@ -34,3 +48,37 @@ computed: {
       return combined.sort();
     }
   },
+  methods: {
+    resetBuilder() {
+      this.range = 'melee';
+      this.selectedAncestry = '';
+      this.adjustements = {
+        proficiency: 0,
+        die: 0,
+        hands: 0,
+        reload: 0,
+        volley: 0,
+        range: 0,
+      };
+      this.traits = {
+        onePoint: [],
+        twoPoint: [],
+        threePoint: [],
+      };
+    },
+    copyToClipboard() {
+      const textToCopy = this.allTraits.join(", ");
+      if (textToCopy === "") {
+        alert("No traits selected to copy!");
+        return;
+      }
+
+      navigator.clipboard.writeText(textToCopy).then(() => {
+        alert("Traits copied to clipboard!");
+      }).catch(err => {
+        console.error('Could not copy text: ', err);
+      });
+    }
+  }
+}
+</script>
